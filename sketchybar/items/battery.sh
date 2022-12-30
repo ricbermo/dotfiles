a580360933106521c9ffaf479e2e2adc1ce6940a
Ricardo Berdejo <ricardo@fullstacklabs.co>
2022-12-30 10:00:40 -0500

    battery

--- sketchybar/items/battery.sh
+++ sketchybar/items/battery.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env sh
-
-sketchybar  --add item battery right \
-            --set battery \
-                        icon.font="$FONT:Black:12.0" \
-                        update_freq=10 \
-                        script="$PLUGIN_DIR/battery.sh" \
-           --subscribe battery system_woke
+sketchybar  --add       item        battery     right \
+            --set       battery     script="$PLUGIN_DIR/battery.sh"        \
+                                    update_freq=5    \
+                                    label.font="$FONT:Light:20.0"        \
+                                    icon.font="$FONT:Semibold:12.0"        \
+                                    label.color=$WHITE       \
+                                    icon.color=$WHITE           \
+                                    label.padding_right=8                   \
+            --subscribe battery     system_woke
--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,38 +1,28 @@
 #!/usr/bin/env sh
 
+source "$HOME/.config/sketchybar/icons.sh"
+
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
 if [ $PERCENTAGE = "" ]; then
-  exit 0
+    exit 0
 fi
 
 case ${PERCENTAGE} in
-  [8-9][0-9]|100) 
-    ICON=" "
-    ICON_COLOR=0xffa6da95
+    8[6-9]|9[0-9]|100) ICON=$BATTERY_100
     ;;
-  7[0-9])
-    ICON=" "
-    ICON_COLOR=0xffeed49f
+    [6-7][0-9]|8[0-5]) ICON=$BATTERY_75 
     ;;
-  [4-6][0-9]) 
-    ICON=" "
-    ICON_COLOR=0xfff5a97f
+    [3-5][0-9]) ICON=$BATTERY_50
     ;;
-  [1-3][0-9]) 
-    ICON=" "
-    ICON_COLOR=0xffee99a0
+    [1-2][0-9]) ICON=$BATTERY_25
     ;;
-  [0-9])
-    ICON=" "
-    ICON_COLOR=0xffed8796
-    ;;
+    *) ICON="$BATTERY_0"
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=""
-  ICON_COLOR=0xffeed49f
+    ICON=$BATTERY_CHARGING
 fi
 
-sketchybar --set $NAME icon=$ICON label="${PERCENTAGE}%" icon.color=${ICON_COLOR}
+sketchybar --set $NAME icon="${PERCENTAGE}%" icon.padding_right=-1 label="$ICON"
