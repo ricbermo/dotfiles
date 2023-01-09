d0e239ba7d40db135628e289ace6db3d221246e8
Ricardo Berdejo <ricardo@fullstacklabs.co>
2023-01-09 07:35:29 -0500

    adjust bluetooth and wifi

--- sketchybar/items/bluetooth.sh
+++ sketchybar/items/bluetooth.sh
@@ -1,6 +1,9 @@
-sketchybar  -m --add event bluetooth_change "com.apple.bluetooth.status" \
-            --add item bluetooth right                                   \
-            --set bluetooth script="$PLUGIN_DIR/bluetooth.sh"            \
-                           click_script="$PLUGIN_DIR/bt_click.sh"        \
-                           update_freq=5                                 \
-            --subscribe bluetooth bluetooth_change
+sketchybar --add alias "Control Center,Bluetooth" right                            \
+           --rename "Control Center,Bluetooth" bluetooth_alias                     \
+           --set bluetooth_alias icon.drawing=off                                  \
+                              label.drawing=off                                    \
+                              alias.color="$ORANGE"                                \
+                              background.padding_right=0                           \
+                              align=right                                          \
+                              click_script="$PLUGIN_DIR/bt_click.sh"               \
+
--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,11 +1,39 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
-SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
-CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+update() {
+  source "$HOME/.config/sketchybar/colors.sh"
+  source "$HOME/.config/sketchybar/icons.sh"
 
-if [ "$SSID" = "" ]; then
-  sketchybar --set $NAME label="Disconnected" icon=􀙈
-else
-  sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=􀙇
-fi
+  CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+  SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+
+  args=()
+  
+  if [ "$SSID" = "" ]; then
+    args+=(--set "$NAME" label="N/A")
+  else
+    args+=(--set "$NAME" label="$SSID" label.drawing=off)
+  fi
+
+  args+=(--set wifi.details label="$SSID" click_script="sketchybar --set $NAME popup.drawing=off")
+
+  sketchybar -m "${args[@]}" > /dev/null
+
+  if [ "$SENDER" = "forced" ]; then
+    sketchybar --animate tanh 15 --set "$NAME" label.y_offset=5 label.y_offset=0
+  fi
+}
+popup() {
+  sketchybar --set "$NAME" popup.drawing="$1"
+}
+
+case "$SENDER" in
+  "routine"|"forced") update
+  ;;
+  "mouse.entered") popup on
+  ;;
+  "mouse.exited"|"mouse.exited.global") popup off
+  ;;
+    "mouse.clicked") popup toggle
+  ;;
+esac
